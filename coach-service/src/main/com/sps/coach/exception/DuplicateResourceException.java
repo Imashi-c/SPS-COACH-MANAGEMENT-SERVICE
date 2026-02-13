a84ed0477b2a71c2e004a52e956cd8701f14f961
Imashi-c <imashigunarathna25@gmail.com>
2026-02-13 13:23:11 +0530

    Update DuplicateResourceException.java

--- coach-service/src/main/com/sps/coach/exception/DuplicateResourceException.java
+++ coach-service/src/main/com/sps/coach/exception/DuplicateResourceException.java
@@ -1,1 +1,18 @@
+package com.sps.coach.exception;
 
+/**
+ * Exception thrown when trying to create a duplicate resource
+ *
+ * @author SPS Cricket Club
+ * @version 1.0.0
+ */
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+    public DuplicateResourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
